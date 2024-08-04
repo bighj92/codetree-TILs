dd4a69a2fcf31b5a502705992eabdd4c230ca715
bighj92 <67255710+bighj92@users.noreply.github.com>
2024-08-04 18:46:30 +0900

    Title: 정수 선언하고 뺄셈, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

--- 240804/정수 선언하고 뺄셈/define-numbers-and-substract.java
+++ 240804/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -3,7 +3,8 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         int a = 97;
         int b = 13;
+        int c = a-b;
 
-        System.out.print(a + " - " + b + " = " + (a-b));
+        System.out.print(a + " - " + b + " = " + c);
     }
 }
