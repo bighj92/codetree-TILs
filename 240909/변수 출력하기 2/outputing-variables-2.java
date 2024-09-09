7b6d8ba03b4b05fb88ba74845dd630d5fd59232c
bighj92 <67255710+bighj92@users.noreply.github.com>
2024-09-09 14:03:18 +0900

    Title: 변수 출력하기 2, Time: 103ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240909/변수 출력하기 2/outputing-variables-2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+
+        int a    = 3;
+        String b = "C";
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(a+"!.....!"+b);
+        bw.flush();
+        bw.close();
+
+
+    }
+}
